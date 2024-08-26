f4fb55b1e48fb0053ac52ed6196de0d32e57308c
leetoneee <letoan7442@gmail.com>
2024-08-26 16:28:46 +0700

    build: add Lato font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,38 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localfont from "next/font/local";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const lato = localfont({
+  src: [
+    {
+      path: '../../public/fonts/Lato-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Lato-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Lato-Italic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/Lato-Semibold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Lato-Bold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+  variable: "--font-lato"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +46,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${lato.variable} font-lato`}>{children}</body>
     </html>
   );
 }
