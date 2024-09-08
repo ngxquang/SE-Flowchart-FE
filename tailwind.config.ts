c0556a46092213c9b411fb79bdbdee38b4d6ded5
Binkeurss <nvbinh271104@gmail.com>
2024-09-09 03:17:50 +0700

    build: add screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,13 @@ const config: Config = {
       fontFamily: {
         lato: ["var(--font-lato)"],
       },
+      screens: {
+        'sm': '640px',   // Small screens
+        'md': '768px',   // Medium screens
+        'lg': '1024px',  // Large screens
+        'xl': '1280px',  // Extra large screens
+        '2xl': '1536px', // 2x Extra large screens
+      },
     },
     colors: {
       'primary': '#2E3C63',
