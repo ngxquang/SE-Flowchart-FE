e00a652531badbc831f418f171302477f2a536d6
chihuy124 <22520577@gm.uit.edu.vn>
2024-09-09 09:29:32 +0700

    import and export file index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,7 +4,9 @@ import ButtonOutline from './Button/ButtonOutline';
 import ButtonSolid from './Button/ButtonSolid';
 import Input from './Input/Input';
 import InputRadio from './Input/InputRadio';
+import Search from './Search/Search';
 
 export { classNames };
 export { ButtonBase, ButtonSolid, ButtonOutline };
 export { Input, InputRadio };
+export {Search};
