467990005e3a63e83222fbab250ae93c019ea1bf
chihuy124 <22520577@gm.uit.edu.vn>
2024-09-09 08:32:31 +0700

    show both header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useContext } from 'react';
 import { AppContext } from '@/contexts';
 import { classNames } from '@/components';
 import HeaderEditor from '@/components/Header/HeaderEditor';
+import HeaderNormal from '@/components/Header/HeaderNormal';
 
 export default function Home() {
   const { state, setState } = useContext(AppContext);
@@ -11,7 +12,9 @@ export default function Home() {
   return (
     <main>
       <HeaderEditor title="Hello everybody I am Huy ye ye" />
+      
       <p>{state}</p>
+      <HeaderNormal/>
       <button className="" onClick={() => setState('world')}>
         change
       </button>
