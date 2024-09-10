cccf6c5d214c032453705c06db3f10a0f3548c29
chihuy124 <22520577@gm.uit.edu.vn>
2024-09-10 16:43:51 +0700

    fix name of file Search/index.tsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,7 +8,7 @@ import ButtonOutline from './Button/ButtonOutline';
 import ButtonSolid from './Button/ButtonSolid';
 import Input from './Input/Input';
 import InputRadio from './Input/InputRadio';
-import Search from './Search/Search';
+import Search from './Search';
 
 export { classNames };
 export { ButtonBase, ButtonSolid, ButtonOutline };
