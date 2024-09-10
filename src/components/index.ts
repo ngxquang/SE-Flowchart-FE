8f69e419833e758d3ed02030e07bdccfbc3ff98f
chihuy124 <22520577@gm.uit.edu.vn>
2024-09-10 16:39:08 +0700

    feat Modal

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,9 +8,11 @@ import ButtonOutline from './Button/ButtonOutline';
 import ButtonSolid from './Button/ButtonSolid';
 import Input from './Input/Input';
 import InputRadio from './Input/InputRadio';
+import Modal_taoBT from './Modal/Modal_taoBT';
 
 export { classNames };
 export { ButtonBase, ButtonSolid, ButtonOutline };
 export { Input, InputRadio };
 export { EBAssignment, EBPseudo };
 export { Preview, PreviewRun };
+export { Modal_taoBT};
