366075c70bfd3a38c8c1ae1f7a115f1f6a5aafa3
nghgi <22520357@gm.uit.edu.vn>
2024-09-12 22:46:15 +0700

    refactor(route layout): recreate folder tree

---
+++ src/app/chapter/page.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+export default function ChapterListScreen() {
+  const chapters = [
+    { id: 1, title: 'Chương 1' },
+    { id: 2, title: 'Chương 2' }
+  ];
+
+  return (
+    <div>
+      <h1>Danh sách chương</h1>
+      <ul>
+        {chapters.map((chapter) => (
+          <li key={chapter.id}>
+            <Link href={`/sv/${chapter.id}`}>{chapter.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
